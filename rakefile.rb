7b4fadd595f7eaf3d64b07135e60aab94282581f
hvadehra <hvadehra@gmail.com>
2018-02-21 13:29:47 +0530

    # rake

--- rakefile.rb
+++ rakefile.rb
@@ -10,20 +10,26 @@ require 'jenkins_api_client'
 STDIN.sync=true; STDOUT.sync=true
 
 namespace :git do
-    task :update do
-        res = `git diff "origin/#{RELEASE_BRANCH}"`.strip
-        puts res
-        if res != ''
+    task :check_remote_diff do
+        diff = `git diff "origin/#{RELEASE_BRANCH}"`.strip
+        if !diff.empty?
             error("not in sync with remote")
         end
+    end
+end
+
+namespace :mvn do
+    task :release do
+        puts "performing release"
+        sh %Q{mvn clean release:prepare release:perform}
     end
 end
 
 task :release do
     cur_branch = `git rev-parse --abbrev-ref HEAD`.strip
     if cur_branch == "#{RELEASE_BRANCH}"
-        puts "releasing from #{cur_branch}"
-        invoke_task "git:update"
+        #invoke_task "git:check_remote_diff"
+        invoke_task "mvn:release"
     else
         error("not in release branch")
     end
